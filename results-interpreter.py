eebbc7b55ce39230073391a7e9785cc443fcde30
slomkarafa <slomkarafa@gmail.com>
2018-05-09 07:51:09 +0200

    endless connection try

--- results-interpreter.py
+++ results-interpreter.py
@@ -6,9 +6,9 @@ from src import config
 
 def main():
     _serial = serial.Serial()
-    filename = ''
+    filename = '1'
     while 1:
-        filename = input("Enter output filename\n")
+        # filename = input("Enter output filename\n")
 
         _serial.read_to_file("{}{}.csv".format(config.OUT_PATH, filename))
 
--- src/serial.py
+++ src/serial.py
@@ -12,20 +12,32 @@ def get_port():
         try:
             s = serial.Serial(port)
             s.close()
-            print("Available port is {}".format(port))
+            # print("Available port is {}".format(port))
             return port
         except (OSError, serial.SerialException) as e:
-            print("Connection error \n" + e)
+            pass
+            # print("Connection error \n" + str(e))
 
 
 class Serial(serial.Serial):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.port = get_port()
+        self.port = "/dev/ttyACM0"
         self.baudrate = config.baudrate
         self.parity = config.parity
         self.stopbits = config.stopbits
         self.bytesize = config.bytesize
+        self.try_to_open()
+
+    def try_to_open(self):
+        while 1:
+            try:
+                self.port = get_port()
+                self.open()
+                break
+            except Exception:
+                print("Unsuccessful connection try")
+                time.sleep(1)
 
     def read_to_file(self, filename="0"):
         os.makedirs(os.path.dirname(filename), exist_ok=True)
@@ -39,7 +51,7 @@ class Serial(serial.Serial):
                 out = str(temp.decode()).replace("\00", "")
                 if out != '':
                     if out != config.STOP:
-                        file.write(out)
+                        # file.write(out)
                         print(out)
                     else:
                         print("File has been saved")
